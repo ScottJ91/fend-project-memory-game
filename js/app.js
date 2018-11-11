477de94f026362b815ba81fa8c50c4d71360c616
Scott Jensen <scottj91.sj@gmail.com>
2018-11-11 16:59:03 -0700

    Update js/app.js
    Added variables

--- js/app.js
+++ js/app.js
@@ -1,7 +1,64 @@
 /*
  * Create a list that holds all of your cards
  */
+var cards = [];
+var cardType = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
+var flipCards = [];
 
+var hours = 0;
+var minutes = 0;
+var seconds = 0;
+var stopGame = 0;
+
+window.onload = function() {
+    setInterval(function() {
+        if (stopGame !== 1) {
+            seconds++;
+            if (seconds === 60) {
+                minutes++;
+                seconds = 0;
+            }
+            if (minutes === 60) {
+                hours++;
+                minutes = 0;
+                seconds = 0;
+            }
+            $('.timer').html(hours + ':' + minutes + ':' + seconds);
+
+            console.log(minutes);
+            console.log(seconds);
+        }
+
+    }, 800);
+};
+
+$('.deck').each(function() {
+    $(this).find('li').each(function() {
+        cards.push($(this));
+    });
+});
+
+var temp = 0;
+
+cardType = shuffle(cardType);
+
+var cardNum = 0;
+$('.deck').each(function() {
+    $(this).find('li').find('i').each(function() {
+        $(this).removeAttr('class');
+        $(this).addClass(cardType[cardNum]);
+        cardNum++;
+    });
+});
+
+$('.deck').each(function() {
+    $(this).find('li').find('i').each(function() {
+        var tempClass = $($(cards[temp][0]).find('i')[0]).attr('class');
+        $(this).removeAttr('class');
+        $(this).addClass(tempClass);
+        temp++;
+    });
+});
 
 /*
  * Display the cards on the page
@@ -12,7 +69,8 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,14 +83,48 @@ function shuffle(array) {
     return array;
 }
 
+var moves = 0,
+    stars = 3;
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+removeProperties = function(prop) {
+    setTimeout(function() {
+        prop.removeClass('show open animated wobble');
+        flipCards[0].removeClass('show open animated wobble');
+        flipCards = [];
+    }, 400);
+};
+
+showCardOnClick = function(clickEvent) {
+        clickEvent.on('click', function() {
+                    moves++;
+                    if (moves === 16) {
+
+                    } else if (moves > 16 && moves <= 25) {
+                        $('section ul li').hide();
+                        $('section ul').append('<li><i class="fa fa-star"></i></li>');
+                        $('section ul').append('<li><i class="fa fa-star"></i></li>');
+                        stars = 2;
+                    } else if (moves > 25) {
+                        $('section ul li').hide();
+                        $('section ul').append('<li><i class="fa fa-star"></i></li>');
+                        stars = 1;
+                    }
+
+                    $('.moves').html(moves);
+                    if ((flipCards.length % 2) === 0) {
+                        $(this).addClass('show open animated wobble');
+                        $(this).off('click');
+                        flipCards.push($(this));
+                    } else if (flipCards.length !== 0) {
+                        $(this).addClass('show open animated wobble');
+
+                        /*
+                         * set up the event listener for a card. If a card is clicked:
+                         *  - display the card's symbol (put this functionality in another function that you call from this one)
+                         *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+                         *  - if the list already has another card, check to see if the two cards match
+                         *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+                         *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+                         *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+                         *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+                         */
