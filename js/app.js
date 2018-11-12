608a973789d6766273b0080fc7e1f920d92f3c20
Scott Jensen <scottj91.sj@gmail.com>
2018-11-11 17:08:06 -0700

    Update js/app.js
    variable self

--- js/app.js
+++ js/app.js
@@ -118,13 +118,17 @@ showCardOnClick = function(clickEvent) {
                     } else if (flipCards.length !== 0) {
                         $(this).addClass('show open animated wobble');
 
-                        /*
-                         * set up the event listener for a card. If a card is clicked:
-                         *  - display the card's symbol (put this functionality in another function that you call from this one)
-                         *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-                         *  - if the list already has another card, check to see if the two cards match
-                         *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-                         *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-                         *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-                         *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-                         */
+                        var self = $(this);
+                        for (var i = 0; i < flipCards.length; i++) {
+                            if (flipCards[i].find('i').attr('class') === self.find('i').attr('class')) {
+
+                                /*
+                                 * set up the event listener for a card. If a card is clicked:
+                                 *  - display the card's symbol (put this functionality in another function that you call from this one)
+                                 *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+                                 *  - if the list already has another card, check to see if the two cards match
+                                 *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+                                 *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+                                 *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+                                 *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+                                 */
