914752955805ee79d027e7fc4a218ba02988ffa5
Scott Jensen <scottj91.sj@gmail.com>
2018-11-13 00:50:53 -0700

    Update js/app.js
    added end game window/restart

--- js/app.js
+++ js/app.js
@@ -139,14 +139,37 @@ showCardOnClick = function(clickEvent) {
                         }
                     }
 
+                    if ($('.deck').find('.match').length === 16) {
+                        setTimeout(function() {
+                            $('.deck').each(function() {
 
-                    /*
-                     * set up the event listener for a card. If a card is clicked:
-                     *  - display the card's symbol (put this functionality in another function that you call from this one)
-                     *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-                     *  - if the list already has another card, check to see if the two cards match
-                     *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-                     *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-                     *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-                     *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-                     */
+                                swal({
+                                    title: 'Good Job',
+                                    type: 'success',
+                                    text: 'You WIN!  Moves taken ' + moves + '. Stars received ' + stars + '. Elapsed time: ' + hours + ' Hours ' + minutes + ' Minutes and ' + seconds + ' Seconds',
+                                    allowOutsideClick: false,
+                                    showCancelButton: true,
+                                    confirmButtonText: 'Try Again',
+                                    confirmButtonColor: '#008000',
+                                    cancelButtonText: 'Exit',
+                                    cancelButtonColor: '#FF0000'
+                                }).then(function() {
+                                    location.reload();
+                                }, function(dismiss) {
+                                    console.log('Yes');
+
+                                });
+
+                            });
+                        }, 300);
+
+                        /*
+                         * set up the event listener for a card. If a card is clicked:
+                         *  - display the card's symbol (put this functionality in another function that you call from this one)
+                         *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+                         *  - if the list already has another card, check to see if the two cards match
+                         *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+                         *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+                         *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+                         *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+                         */
